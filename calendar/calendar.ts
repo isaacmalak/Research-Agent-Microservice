865e16becaed78bc3713f5d60bf34588a9b4fe9f
Isaac Malak <isaacmalak123@gmail.com>
2026-07-24 20:53:39 +0300

    fix: add status code to response in webhook route

--- calendar/calendar.ts
+++ calendar/calendar.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   console.log(req);
-  res.json({ message: "Hello from the webhook route" });
+  res.status(200).json({ message: "Hello from the webhook route" });
 });
 
 export default router;
