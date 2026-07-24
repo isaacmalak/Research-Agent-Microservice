f4d45c8ec33f058884769629f16f09641277f015
Isaac Malak <isaacmalak123@gmail.com>
2026-07-24 20:58:29 +0300

    fix: update webhook route logging to a static message

--- calendar/calendar.ts
+++ calendar/calendar.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(req);
+  console.log("This is the webhook route");
   res.status(200).json({ message: "Hello from the webhook route" });
 });
 
